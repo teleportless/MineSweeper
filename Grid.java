9a7d68540b4544c45ceaa95624640ddb68bf54e3
teleportless <teleportless@gmail.com>
2025-09-01 23:40:37 -0400

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -1,37 +1,53 @@
-public class Grid {
-private int nearbyMines = 0;
-private boolean isRevealed = false;
-private boolean isFlagged = false;
-private boolean isMine = false;
+import java.awt.Color;
+import javax.swing.JButton;
 
-public int getNearbyMines() {
-    return nearbyMines;
-}
-public void setNearbyMines(int nearbyMines) {
-    this.nearbyMines = nearbyMines;
-}
-public boolean isRevealed() {
-    return isRevealed;
-}
-public void setRevealed(boolean isRevealed) {
-    this.isRevealed = isRevealed;
-}
-public boolean isFlagged() {
-    return isFlagged;
-}
-public void setFlagged() {
-    this.isFlagged = !this.isFlagged;
-}
-public boolean isMine() {
-    return isMine;
-}
-public void setMine(boolean isMine) {
-    this.isMine = isMine;
-}
-public void reset() {
-    this.nearbyMines = 0;
-    this.isRevealed = false;
-    this.isFlagged = false;
-    this.isMine = false;
-}
+public class Grid extends JButton{
+
+    //Attributes
+    private int nearbyMines = 0;
+    private boolean isRevealed = false;
+    private boolean isFlagged = false;
+    private boolean isMine = false;
+    private Color originalColor;
+
+    //Getters
+    public int getNearbyMines() {
+        return nearbyMines;
+    }
+    public boolean isRevealed() {
+        return isRevealed;
+    }
+    public boolean isFlagged() {
+        return isFlagged;
+    }
+    public boolean isMine() {
+        return isMine;
+    }
+
+    //Setters
+    public void setNearbyMines(int nearbyMines) {
+        this.nearbyMines = nearbyMines;
+    }
+    public void setRevealed() {
+        this.isRevealed = true;
+    }
+    public void setFlagged() {
+        this.isFlagged = !this.isFlagged;
+    }
+    public void setMine() {
+        this.isMine = true;
+    }
+    public void setColor(Color color){
+        this.originalColor = color;
+    }
+    public void setOriginalColor(){
+        this.setBackground(this.originalColor);
+    }
+
+    public void reset() {
+        this.nearbyMines = 0;
+        this.isRevealed = false;
+        this.isFlagged = false;
+        this.isMine = false;
+    }
 }
